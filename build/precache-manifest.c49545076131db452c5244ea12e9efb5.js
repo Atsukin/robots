d9338df470b47dec35d63ba713bfa0fbf8c92568
atsukin <atsuki.tamura.2740@gmail.com>
2020-05-07 15:04:27 +0900

    add robot apps

---
+++ build/precache-manifest.c49545076131db452c5244ea12e9efb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c37de481ffbaf4de327f929b3064030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91e517f6d00848f8450c",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "43954106b0b5c1c35fd2",
+    "url": "/static/css/main.765e4c28.chunk.css"
+  },
+  {
+    "revision": "91e517f6d00848f8450c",
+    "url": "/static/js/2.ed586341.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ed586341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43954106b0b5c1c35fd2",
+    "url": "/static/js/main.0cebd59e.chunk.js"
+  },
+  {
+    "revision": "375225690d3392b02cad",
+    "url": "/static/js/runtime-main.1bf37ef1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
